017b494da00c29d991da8b0ad7f7e03b8092f9a0
Jackson <andreyivanov160@mail.ru>
2026-02-23 15:10:53 +0300

    tasks

--- 1.JavaSyntax/src/com/javarush/task/pro/task05/task0507/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0507/Solution.java
@@ -11,5 +11,20 @@ public class Solution {
 
     public static void main(String[] args) throws Exception {
         //напишите тут ваш код
+        Scanner sc = new Scanner(System.in);
+        int numberInput = sc.nextInt();
+        array = new int[numberInput];
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        System.out.println(max);
     }
 }
---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task05/task0511/Solution.java
@@ -0,0 +1,22 @@
+package com.javarush.task.pro.task05.task0511;
+
+import java.util.Scanner;
+
+/* 
+Создаем двумерный массив
+*/
+
+public class Solution {
+    public static int[][] multiArray;
+
+    public static void main(String[] args) {
+        //напишите тут ваш код
+        Scanner sc = new Scanner(System.in);
+        int numberInput = sc.nextInt();
+
+        for (int i = 0; i < numberInput; i++) {
+
+        }
+
+    }
+}
