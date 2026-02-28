f30ec2e3925354d230e607bdde3b009666105570
Jackson <andreyivanov160@mail.ru>
2026-02-28 16:08:14 +0300

    tasks

---
+++ 1.JavaSyntax/src/com/javarush/task/pro/task07/task0701/Solution.java
@@ -0,0 +1,17 @@
+package com.javarush.task.pro.task07.task0701;
+
+/* 
+Все возможные типы
+*/
+
+public class Solution {
+    //напишите тут ваш код
+    byte aByte = 1;
+    short aShort = 2;
+    int anInt = 5;
+    double aDouble = 1.3;
+    float aFloat = 1.4f;
+    long aLong = 100L;
+    char aChar = 'A';
+    boolean aBoolean = true;
+}
